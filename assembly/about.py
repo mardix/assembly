505c38e9b18624828ccdacc0f4201106512f9655
Mardix <mardix@pylot.io>
2019-11-18 02:12:03 -0500

    upd

--- assembly/about.py
+++ assembly/about.py
@@ -7,14 +7,12 @@ About file
 
 __title__ = "Assembly"
 __version__ = "0.3.0"
-__summary__ = """
-Assembly is a pythonic object-oriented, mid stack, batteries included framework built on Flask, that adds structure to your Flask application, and group your routes by class.
-Assembly allows developers to build web applications in much the same way they would build any other object-oriented Python program.
-Technically Assembly Makes Flask Great Again!
-"""
+__summary__ = "Assembly is a pythonic object-oriented, mid stack, batteries included framework built on Flask, that adds structure to your Flask application, and group your routes by class."\
+              "Assembly allows developers to build web applications in much the same way they would build any other object-oriented Python program."\
+              "Technically Assembly Makes Flask Great Again!"
+
 __uri__ = "https://github.com/mardix/assembly"
 __author__ = "Mardix"
 __email__ = "mcx2082@gmail.com"
 __license__ = "MIT"
 __copyright__ = "(c) 2020 %s" % __author__
-
